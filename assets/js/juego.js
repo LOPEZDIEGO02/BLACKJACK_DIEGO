08b2fc2f38faf7c017a32c01d5e1a5f63359abaf
Hiram Méndez <hirammendez000@gmail.com>
2025-05-14 09:48:01 -0600

    btnDetenr Function

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,11 +1,14 @@
-// const btnNuevo = document.querySelector("#btnNuevo")
+// Referencias HTML
+const btnNuevo = document.querySelector("#btnNuevo")
 const btnPedir =document.querySelector("#btnPedir")
 const btnDetener = document.querySelector("#btnDetener")
+const puntosJugadorHtml = document.querySelector('#puntos-jugador-html');
 
 let cartas = ['C','D','H','S'];
 let especiales = ['A', 'J', 'Q', 'K'];
 let deck = [];
 let puntosJugador = 0;
+let puntosComputadora = 0;
 
 function crearDeck() {
     for(const carta of cartas) {
@@ -26,7 +29,7 @@ crearDeck ();
 deck = _.shuffle(deck)
 console.log(deck)
 
-
+// Funciones
 // Esta funcion recibe una carta y retorna un valor
 // ejemplo: 3,10,11...
 function obtenerValorCarta( carta ) {
@@ -58,27 +61,50 @@ btnPedir.addEventListener('click', () => {
     
 
     console.log("tu total de puntos es:", puntosJugador)
-
+    puntosJugadorHtml.innerHTML = puntosJugador;
 
     
     if (puntosJugador > 21 ) {
         console.error("perdiste")
+        btnPedir.disabled = true
+        btnDetener.disabled = true
     } else if (puntosJugador === 21 ){
         console.warn("21 yei")
+        btnPedir.disabled = true
+        btnDetener.disabled = true
     } else {
         console.log("ahorita vemos pae")
     }
+});
+
+btnDetener.addEventListener('click', () => {
+    console.log('btnDetener');
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
 
 
+    // turno computadora
+    const carta = deck.pop()
+    puntosComputadora = puntosComputadora + obtenerValorCarta(carta)
+
+
+     // creo mi imagen
+    const milimagen = document.createElement("img");
+    milimagen.src=`assets/cartas/${carta}.png`
+    milimagen.className="carta"
+
+    // inserto mi imagen
+    document.querySelector("#computadora-cartas").append(milimagen)
 
 
     
+    if (puntosComputadora > puntosJugador) {
+        console.log('computadora gana')
+    } else {
+        console.log('jugador gana')
+    }
 
 
-
-
-
-});
-
-
+    // aqui es cuando nosotros ejecutamos a la computadora por primera vez
+})
 
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,1 +1,25 @@
-console.log("hola")
+if (edad >= 18) {
+  console.log("Es mayor de edad.");
+
+  if (tieneLicencia) {
+    console.log("Tiene licencia para conducir.");
+
+    if (carroDisponible) {
+      console.log("Puede conducir el carro.");
+    } else {
+      console.log("No hay carro disponible.");
+    }
+  } else {
+    console.log("No tiene licencia para conducir.");
+  }
+} else {
+  console.log("Es menor de edad.");
+}
+
+if (edad >= 18) {
+    console.log('es mayor de edad')
+} else if(tieneLicencia) {
+    console.log('tiene licencia')
+} else if (carroDisponible) {
+    console.log('puede conducior')
+}
