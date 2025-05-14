d9728e10856154a1da772f2417c5c79647499133
Diego <digale227@gmail.com>
2025-05-14 07:47:13 -0600

    blackjack

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,6 +5,7 @@ const btnDetener = document.querySelector("#btnDetener")
 let cartas = ['C','D','H','S'];
 let especiales = ['A', 'J', 'Q', 'K'];
 let deck = [];
+let puntosJugador = 0;
 
 function crearDeck() {
     for(const carta of cartas) {
@@ -45,17 +46,32 @@ btnPedir.addEventListener('click', () => {
 
     if( !isNaN(cartaCortada) ) {
         console.log("es un numero", cartaCortada)
+        puntosJugador = puntosJugador + Number(cartaCortada);
 
-    }  {
+    } else  {
         if ( cartaCortada == "A") {
             console.log("Es un As", cartaCortada);
             console.log(11)
+             puntosJugador = puntosJugador + 11;
         } else {
             console.log("Es una carta especial", cartaCortada);
             console.log(10)
+             puntosJugador = puntosJugador + 10;            
         }
     }
-        
+      
+
+    console.log("tu total de puntos es:", puntosJugador)
+
+    if (puntosJugador > 21 ) {
+        console.error("perdiste")
+    } else {
+        if (puntosJugador === 21 ) {
+            console.warn("21 yei")
+        } else {
+            console.log("ahorita vemos pae")
+        }
+    }
     
 
 
