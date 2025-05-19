cd329f7086b4bbcc8ab234f67c8a0d0763ed707d
Hiram Méndez <hirammendez000@gmail.com>
2025-05-19 08:09:46 -0600

    explicacion de ciclos

---
+++ assets/01-explicaciones/01-ciclos.js
@@ -0,0 +1,22 @@
+
+
+let montoAhorrado = 150;
+const montoDeseado = 100;
+
+while ( montoDeseado > montoAhorrado ) {
+    console.log('while')
+
+    console.log('Diego ahorra 10 varos')
+    montoAhorrado += 10;
+    console.log('diego lleva: ', montoAhorrado)
+}
+
+
+
+
+do {
+
+    console.log('Diego ahorra 10 varos')
+    montoAhorrado += 10;
+    console.log('diego lleva: ', montoAhorrado)
+} while( montoDeseado > montoAhorrado);
